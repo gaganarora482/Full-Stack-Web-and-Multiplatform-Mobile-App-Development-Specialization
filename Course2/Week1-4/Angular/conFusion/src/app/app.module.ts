3505b1d2eb7919c9cc0377614d1558a7f0524b52
gkarora <gkarora@iu.edu>
2020-07-16 21:44:12 -0400

    Angular component part 2

--- Course2/Week1-4/Angular/conFusion/src/app/app.module.ts
+++ Course2/Week1-4/Angular/conFusion/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -20,6 +25,9 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
     FlexLayoutModule
   ],
   providers: [],
