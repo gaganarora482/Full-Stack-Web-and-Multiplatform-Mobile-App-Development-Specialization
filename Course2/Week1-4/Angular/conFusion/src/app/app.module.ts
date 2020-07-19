5be836b72713b9225b6b3d4b106bf13e8e8266e3
gkarora <gkarora@iu.edu>
2020-07-19 17:11:48 -0400

    Added aouting module

--- Course2/Week1-4/Angular/conFusion/src/app/app.module.ts
+++ Course2/Week1-4/Angular/conFusion/src/app/app.module.ts
@@ -11,20 +11,27 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishService } from './services/dish.service';
 
-
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -32,6 +39,7 @@ import { FooterComponent } from './footer/footer.component';
     MatToolbarModule,
     MatListModule,
     MatGridListModule,
+    AppRoutingModule,
     MatCardModule,
     MatButtonModule,
     FlexLayoutModule
