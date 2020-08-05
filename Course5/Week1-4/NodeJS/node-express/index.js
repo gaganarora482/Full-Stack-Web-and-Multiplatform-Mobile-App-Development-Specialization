9d07e7cee8763c19d8f593226d17048a11c787a2
gkarora <gkarora@iu.edu>
2020-08-04 23:23:30 -0400

    Assignement 2 and express generator setup

--- Course5/Week1-4/NodeJS/node-express/index.js
+++ Course5/Week1-4/NodeJS/node-express/index.js
@@ -3,6 +3,8 @@ http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 
 const hostname = 'localhost';
@@ -15,6 +17,8 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use((req, res, next) => {
   /*console.log(req.headers);*/
--- Course5/Week1-4/NodeJS/node-express/routes/dishRouter.js
+++ Course5/Week1-4/NodeJS/node-express/routes/dishRouter.js
@@ -25,4 +25,26 @@ dishRouter.route('/')
     res.end('Deleting all dishes');
 });
 
+dishRouter.route('/:dishId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send the following dish: '+req.params.dishId);
+})
+.post((req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description+' where dish id=' + req.params.dishId);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dish/dishId');
+})
+.delete((req, res, next) => {
+    res.end('Deleting following dish '+req.params.dishId);
+});
+
+
+
 module.exports = dishRouter;
